46ccd8f9661cc5a1eeeeb55918618e068edb10c6
Sheldon Von <724530250@qq.com>
2020-07-08 15:38:13 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
- #!/bin/sh
+#!/bin/sh
 
- cd "$(dirname "$(find / -type f -name HomeControl.py | head -1)")"
- python3 HomeControl.py
+cd "$(dirname "$(find / -type f -name HomeControl.py | head -1)")" && python3 HomeControl.py
